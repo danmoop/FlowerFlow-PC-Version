a8856c8120b8ed718e46705195c209edbe0abec8
dan322 <dandurnev0@gmail.com>
2018-02-16 22:21:38 +0300

    trying to fix language problem in repository

--- app.js
+++
@@ -1,8 +0,0 @@
-const BrowserWindow = require('electron').remote.BrowserWindow;
-
-const newWindowBtn = document.getElementById('getstartedbtn')
-
-newWindowBtn.addEventListener('click', function (event) {
-  let win = new BrowserWindow({ width: 1280, height: 768 });
-  win.loadURL('https://www.google.ru');
-});
